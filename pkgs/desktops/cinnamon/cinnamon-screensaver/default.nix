b2b92b2a1e9af36c03f856d16feb37ffe683f65a
Bobby Rong <rjl931189261@126.com>
2023-11-19 23:00:25 +0800

    cinnamon.cinnamon-screensaver: 5.8.1 -> 6.0.0
    https://github.com/linuxmint/cinnamon-screensaver/compare/5.8.1...6.0.0

--- pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-screensaver";
-  version = "5.8.1";
+  version = "6.0.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-d7h9OJ39HVQNCHNr13M1ybDFoU3Xnd1PEczGLHZU/lU=";
+    hash = "sha256-5hXhCPXC7b2SsmvNSLDe/WYQcufN7FfhnaAgTNtqg0I=";
   };
 
   nativeBuildInputs = [
